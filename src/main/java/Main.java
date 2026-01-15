2796a1bd05c2c49f2de7e640d8fef05bc915ca66
MatsVestvik <mats.o.vestvik@gmail.com>
2026-01-15 16:31:58 +0100

    program adds book to local database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BookDAO bookDAO = new BookDAO();
+        Book newBook = new Book(1); // Example book with ID 1
+        
+        try {
+            int bookId = bookDAO.addBook(newBook);
+            if (bookId != -1) {
+                System.out.println("Book added with ID: " + bookId);
+            } else {
+                System.out.println("Failed to add book.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
